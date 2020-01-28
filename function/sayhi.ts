3d6d297103f5c97ca45bbcde82bb4fdc954b268d
liviam8 <48126141+liviam8@users.noreply.github.com>
2020-01-28 15:36:03 +0100

    Functions
    Functions

---
+++ function/sayhi.ts
@@ -0,0 +1,19 @@
+
+//normal function statement
+function sayHi(){
+console.log("hi");
+
+}
+
+sayHi();
+
+
+//functional expression 
+var SayBye = function (){
+
+    console.log('bye');
+
+};
+SayBye();
+
+
