f6ee8699d9843107e3534a65fbe9f5444a6ba715
Livia Mate <liviamate8@gmail.com>
2021-04-17 14:20:34 +0200

    printeven firs version

---
+++ ExpressionAndControlFlow/printeven.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+let numberofexec: number = 500;
+
+for (let index = 0; index <numberofexec ; index++) 
+    
+    if (index % 2 === 0) {
+    console.log (index);   
+}
