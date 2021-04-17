45517cf76b1c86d3fde75c1ac4c4cc6d3fd907c6
Livia Mate <liviamate8@gmail.com>
2021-04-17 16:38:58 +0200

    parametric task

---
+++ ExpressionAndControlFlow/parametric-average.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let input: number= 5;
+let sumary: number=0;
+
+
+for (let index = sumary = 0; index <= input; index++) {
+    sumary += index;
+    
+
+}
+console.log(sumary);
+
+console.log(sumary/input);
